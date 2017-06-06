1d8ee0aabdeae5effd62b93baa122420128eab47
['Pusztai', 'Ryan'] <ryan.pusztai@gentex.com>
2017-06-06 16:29:27 -0500

    Reworked routes file to simplify.

--- routes/routes.js
+++ routes/routes.js
@@ -3,43 +3,29 @@ const apiRouter = express.Router();
 const config = require( "../config" );
 const UsersController = require( "../controllers/user" );
 
-var appRouter = function( app )
+apiRouter.get( "/user/:id", function( req, res )
 {
-	//app.get( "/", function( req, res )
-	//{
-		//res.send("Hello World");
-	//} );
-
-	app.get( "/api/user/:id", function( req, res )
+	var accountMock =
 	{
-		var accountMock =
-		{
-			"username": "peter.griffin",
-			"password": "1234",
-			"email": "peter.griffin@gmail.com"
-		};
+		"username": "peter.griffin",
+		"password": "1234",
+		"email": "peter.griffin@gmail.com"
+	};
 
-		if( !req.query.username )
-		{
-			return res.send( { "status": "error", "message": "missing username" } );
-		}
-		else if ( req.query.username != accountMock.username )
-		{
-			return res.send( { "status": "error", "message": "wrong username" } );
-		}
-		else
-		{
-			return res.send( accountMock );
-		}
-	} );
-
-	app.post( "/api/user/login",  UsersController.Login );
-
-	// Angular.js Application -------------------------------------------------
-    app.get( "*", function( req, res )
+	if( !req.query.username )
 	{
-        res.sendFile( "index.html", { root: __dirname + "/static" } ); // load the single view file (angular will handle the page changes on the front-end)
-    } );
-};
+		return res.send( { "status": "error", "message": "missing username" } );
+	}
+	else if ( req.query.username != accountMock.username )
+	{
+		return res.send( { "status": "error", "message": "wrong username" } );
+	}
+	else
+	{
+		return res.send( accountMock );
+	}
+} );
 
-module.exports = appRouter;
+apiRouter.post( "/user/login",  UsersController.Login );
+
+module.exports = apiRouter;
