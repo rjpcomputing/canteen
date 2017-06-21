45700d22077ed4336bf5b23650a28f4c54cf19ed
Pusztai <ryan.pusztai@localhost.localdomain>
2017-06-20 23:00:30 -0400

    Adds #3 - Add "Are you sure" message box to all deletes. Moved all directives together into a directives file.

--- static/setup/setup.js
+++ static/setup/setup.js
@@ -80,28 +80,4 @@ function( $scope, $state, Customer )
 	} );
 } ])
 
-// allow you to format a text input field.
-// <input type="text" ng-model="test" format="number" />
-// <input type="text" ng-model="test" format="currency" />
-.directive( "format", ["$filter", function ( $filter )
-{
-	return {
-		require: "?ngModel",
-		link: function ( scope, elem, attrs, ctrl )
-		{
-			if ( !ctrl ) return;
-
-			ctrl.$formatters.unshift( function ( a )
-			{
-				return $filter( attrs.format )( ctrl.$modelValue );
-			} );
-
-			elem.bind( "blur", function ( event )
-			{
-				var plainNumber = elem.val().replace( /[^\d|\-+|\.+]/g, "" );
-				elem.val( $filter( attrs.format )( plainNumber ) );
-			} );
-		}
-	};
-}] )
 ;
