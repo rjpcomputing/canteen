ac996c2d66c3a32963cf30c5704ae96885420d99
Pusztai, Ryan <ryan.pusztai@gentex.com>
2017-06-22 13:47:47 -0400

    Remove stale comment.

--- static/app.js
+++ static/app.js
@@ -1,7 +1,7 @@
 ( function()
 {
 	var appVersion = "17.06-03";
-	//
+	
 	angular.module( "CanteenApp", [ "Canteen.Login", "Canteen.Store", "Canteen.ShopDialog", "Canteen.Event", "Canteen.Customer", "Canteen.PurchaseItemDialog", "Canteen.Setup", "Canteen.Supply", "Canteen.CustomersTable", "Canteen.About", "Canteen.Directives", "ui.router", "ui.bootstrap", "angular-loading-bar", "ngAnimate", "ngCookies" ] )
 
 	.constant( "CanteenSettings", { staffAboveCost: 0, canteenDiscount: 50 } )
