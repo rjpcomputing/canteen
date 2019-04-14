f1d0a8ed73df349a34512d3a50ca54b32cefa4b0
RJP Computing <rjpcomputing@gmail.com>
2019-04-14 20:01:15 +0000

    Added Lightsail hosting files.

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps : [{
+    name: 'canteen',
+    script: 'server.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: '',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development',
+      DATABASE_HOST: 'localhost',
+      DATABASE: 'canteen_production'
+    }
+  }]
+};
