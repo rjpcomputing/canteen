42cca37224c43b253fbc837099960a311a5b4ff1
RJP Computing <rjpcomputing@gmail.com>
2019-04-29 16:04:27 -0400

    Format changes.

--- models/event.js
+++ models/event.js
@@ -10,12 +10,12 @@ module.exports = function( db ) {
 	EventModel.GetCustomers = function( eventId, timeZone ) {
 		console.log( "[DEBUG] timezone:", timeZone );
 		let query =
-		`SELECT customer.id as id, customer.created_at as created_at, customer.updated_at as updated_at, customer.name as name, customer.starting_balance as starting_balance, customer.balance as balance,
+			`SELECT customer.id as id, customer.created_at as created_at, customer.updated_at as updated_at, customer.name as name, customer.starting_balance as starting_balance, customer.balance as balance,
 			(SELECT COUNT( * ) FROM purchase WHERE customer_id = customer.id AND DATE( created_at AT TIME ZONE 'UTC' AT TIME ZONE $2 ) = DATE( now() AT TIME ZONE $2 ) ) as purchases_today,
 			event_customer.type_id as type_id, (SELECT type FROM customer_type WHERE id = event_customer.type_id) as type
-		FROM event_customer, customer
-		WHERE event_customer.event_id = $1 AND event_customer.customer_id = customer.id
-		ORDER BY name ASC`;
+			FROM event_customer, customer
+			WHERE event_customer.event_id = $1 AND event_customer.customer_id = customer.id
+			ORDER BY name ASC`;
 
 		return db.any( query, [eventId, timeZone || "EDT"] );
 	};
@@ -33,7 +33,7 @@ module.exports = function( db ) {
 	};
 
 	EventModel.AddCustomer = function( eventId, customerId, typeId ) {
-		return db.one( "INSERT INTO event_customer ( event_id, customer_id, type_id ) VALUES( $1, $2, $3 ) RETURNING id", [ eventId, customerId, typeId ] );
+		return db.one( "INSERT INTO event_customer ( event_id, customer_id, type_id ) VALUES( $1, $2, $3 ) RETURNING id", [eventId, customerId, typeId] );
 	};
 
 	EventModel.UpdateCustomer = function( eventCustomer ) {
@@ -41,7 +41,7 @@ module.exports = function( db ) {
 	};
 
 	EventModel.DeleteCustomer = function( eventId, customerId ) {
-		return db.none( "DELETE FROM event_customer WHERE event_id = $1 AND customer_id = $2", [ eventId, customerId ] );
+		return db.none( "DELETE FROM event_customer WHERE event_id = $1 AND customer_id = $2", [eventId, customerId] );
 	};
 
 	EventModel.Update = function( event ) {
